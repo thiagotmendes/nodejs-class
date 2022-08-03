ed9c6d541261500387cb7e0657dbef4d24319ba9
tmendes-ciandt <tmendes@ciandt.com>
2022-08-03 16:02:18 -0300

    Maneja erro

--- http-validacao.js
+++ http-validacao.js
@@ -1,13 +1,21 @@
 import fetch from "node-fetch";
 
+function manejaErros(erro) {
+    throw new Error(erro.message)
+}
+
 async function checkStatus(arrayURL) {
-    const arrayStatus = await Promise
+    try {
+        const arrayStatus = await Promise
         .all(arrayURL
             .map(async url => {
                 const res = await fetch(url)
                 return res.status;
             })) 
-    return arrayStatus;
+        return arrayStatus;
+    } catch (erro) {
+        manejaErros(erro)
+    }
 }
 
 function geraArrayURL(arrayLinks) {
@@ -17,7 +25,13 @@ function geraArrayURL(arrayLinks) {
 async function validaURL(arrayLinks) { 
     const links =  geraArrayURL(arrayLinks);
     const statusLinks = await checkStatus(links)
-    return statusLinks;
+
+    const resultados = arrayLinks.map((objeto, indice) => ({ 
+        ...objeto, 
+        status: statusLinks[indice]
+    }))
+
+    return resultados;
 }
 
 export default validaURL;
